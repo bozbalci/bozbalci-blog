dbb36e8b8a0815f80763020da47c30a4a935f69b
Berk Ozbalci <github@bozbalci.me>
2025-01-15 21:43:23 +0000

    Add prettier config

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+const config = {
+  "plugins": ["prettier-plugin-jinja-template"],
+  "overrides": [
+    {
+      "files": ["*.html"],
+      "options": {
+        "parser": "jinja-template"
+      }
+    }
+  ]
+};
+
+export default config;

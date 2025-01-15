a78c7c7208b4939e19eb7c1baa85e4be574a34a0
Berk Ozbalci <github@bozbalci.me>
2025-01-15 00:05:58 +0000

    pre-vue

--- notcms/settings/dev.py
+++ notcms/settings/dev.py
@@ -19,7 +19,7 @@ CACHES = {
     }
 }
 
-DJANGO_VITE = {"default": {"dev_mode": True}}
+DJANGO_VITE = {"default": {"dev_mode": True, "dev_server_host": "farringdon.local"}}
 
 DATABASES = {
     "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}

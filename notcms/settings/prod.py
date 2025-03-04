86e55e4c80ac1259a09bf1b55af9afab7b079466
Berk Ozbalci <github@bozbalci.me>
2025-03-04 23:00:16 +0300

    Add Fathom and Sentry

--- notcms/settings/prod.py
+++ notcms/settings/prod.py
@@ -1,3 +1,5 @@
+import sentry_sdk
+
 from .base import *  # noqa: F403
 
 # ruff: noqa: F405
@@ -89,3 +91,21 @@ LOGGING = {
         },
     },
 }
+
+# Sentry
+sentry_sdk.init(
+    dsn=os.getenv("SENTRY_DSN"),
+    # Add data like request headers and IP for users,
+    # see https://docs.sentry.io/platforms/python/data-management/data-collected/
+    # for more info
+    send_default_pii=True,
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for tracing.
+    traces_sample_rate=1.0,
+    _experiments={
+        # Set continuous_profiling_auto_start to True
+        # to automatically start the profiler on when
+        # possible.
+        "continuous_profiling_auto_start": True,
+    },
+)

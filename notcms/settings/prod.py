eb0ea48147bd5fc59edd5c88598c44091e07a9ff
Berk Ozbalci <github@bozbalci.me>
2025-05-11 17:00:52 +0100

    add sentry profiling options

--- notcms/settings/prod.py
+++ notcms/settings/prod.py
@@ -112,6 +112,12 @@ sentry_sdk.init(
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for tracing.
     traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of profile sessions.
+    profile_session_sample_rate=1.0,
+    # Set profile_lifecycle to "trace" to automatically
+    # run the profiler on when there is an active transaction
+    profile_lifecycle="trace",
     _experiments={
         # Set continuous_profiling_auto_start to True
         # to automatically start the profiler on when

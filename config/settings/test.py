33af4d4625bd6005f580be25a8794a2861f08483
Berk Ozbalci <github@bozbalci.me>
2025-05-24 04:18:52 +0100

    compilemessages in Docker

---
+++ config/settings/test.py
@@ -0,0 +1,17 @@
+from .base import *  # noqa: F403
+from .base import STORAGES, TEMPLATES, env
+
+SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+    default="!!!SET DJANGO_SECRET_KEY!!!",
+)
+
+PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
+
+EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
+
+TEMPLATES[0]["OPTIONS"]["debug"] = True  # type: ignore[index]
+
+STORAGES["default"] = {
+    "BACKEND": "django.core.files.storage.FileSystemStorage",
+}
